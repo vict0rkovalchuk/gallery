c7fd113c0731101efaf7a45a94b88d6a777e89a7
Victor <vict02r05@gmail.com>
2021-05-04 00:22:20 +0300

    add oop style

--- scripts.js
+++ scripts.js
@@ -2,81 +2,106 @@
 
 import imagesRefs from "./gallery-items.js";
 
-let ulRef = document.querySelector("ul");
-
-imagesRefs.forEach(({ original }) => {
-  ulRef.innerHTML += `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${original}" data-source="${original}"/></a></li>`;
-});
-
 const refs = {
   gallery: document.querySelector(".js-gallery"),
   modalRef: document.querySelector(".lightbox"),
+  imageRef: document.querySelector(".lightbox__image"),
+  prev: document.querySelector(".btn-prev"),
+  next: document.querySelector(".btn-next"),
+  overlayRef: document.querySelector(".lightbox__overlay"),
+  btn: document.querySelector(".lightbox__button"),
+  ulRef: document.querySelector("ul"),
 };
 
-refs.gallery.addEventListener("click", ongalleryClick);
-
-function ongalleryClick(event) {
-  event.preventDefault();
-
-  if (event.target.nodeName !== "IMG") {
-    return;
+class Gallery {
+  constructor(elements, elementsRefs) {
+    this.imagesRefs = elements;
+    this.refs = elementsRefs;
   }
 
-  refs.modalRef.classList.add("is-open");
-  let imageRef = document.querySelector(".lightbox__image");
-  imageRef.src = event.target.src;
-
-  const slides = document.querySelectorAll(".gallery__image");
-  const arr = Array.from(slides);
-  const prev = document.querySelector(".btn-prev");
-  const next = document.querySelector(".btn-next");
-
-  {
-    let slideIndex = arr.indexOf(event.target);
-
-    const activeSlide = (n) => {
-      imageRef.src = slides[n].src;
-    };
-
-    const nextSlide = () => {
-      if (slideIndex == slides.length - 1) {
-        slideIndex = 0;
-        activeSlide(slideIndex);
-      } else {
-        slideIndex++;
-        activeSlide(slideIndex);
-      }
-    };
-
-    const prevSlide = () => {
-      if (slideIndex == 0) {
-        slideIndex = slides.length - 1;
-        activeSlide(slideIndex);
-      } else {
-        slideIndex--;
-        activeSlide(slideIndex);
-      }
-    };
-
-    next.addEventListener("click", nextSlide);
-    prev.addEventListener("click", prevSlide);
+  newImagesOnPage() {
+    imagesRefs.forEach(({ original }) => {
+      refs.ulRef.innerHTML += `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${original}" data-source="${original}"/></a></li>`;
+    });
   }
 
-  {
-    window.addEventListener("keydown", (event) => {
-      if (event.key === "Escape") {
+  addListener() {
+    refs.gallery.addEventListener("click", ongalleryClick);
+
+    function ongalleryClick(event) {
+      event.preventDefault();
+
+      let slides = document.querySelectorAll(".gallery__image");
+      let arr = Array.from(slides);
+
+      if (event.target.nodeName !== "IMG") {
+        return;
+      }
+
+      refs.modalRef.classList.add("is-open");
+      refs.imageRef.src = event.target.src;
+
+      {
+        let slideIndex = arr.indexOf(event.target);
+
+        const activeSlide = (n) => {
+          refs.imageRef.src = slides[n].src;
+        };
+
+        const nextSlide = () => {
+          if (slideIndex == slides.length - 1) {
+            slideIndex = 0;
+            activeSlide(slideIndex);
+          } else {
+            slideIndex++;
+            activeSlide(slideIndex);
+          }
+        };
+
+        const prevSlide = () => {
+          if (slideIndex == 0) {
+            slideIndex = slides.length - 1;
+            activeSlide(slideIndex);
+          } else {
+            slideIndex--;
+            activeSlide(slideIndex);
+          }
+        };
+
+        refs.next.addEventListener("click", nextSlide);
+        refs.prev.addEventListener("click", prevSlide);
+
+        window.addEventListener("keydown", (event) => {
+          if (event.code === "ArrowRight") {
+            nextSlide();
+          }
+        });
+
+        window.addEventListener("keydown", (event) => {
+          if (event.code === "ArrowLeft") {
+            prevSlide();
+          }
+        });
+      }
+
+      window.addEventListener("keydown", (event) => {
+        if (event.key === "Escape") {
+          refs.modalRef.classList.remove("is-open");
+        }
+      });
+
+      refs.overlayRef.addEventListener("click", () => {
         refs.modalRef.classList.remove("is-open");
-      }
-    });
+      });
 
-    let overlayRef = document.querySelector(".lightbox__overlay");
-    overlayRef.addEventListener("click", () => {
-      refs.modalRef.classList.remove("is-open");
-    });
+      refs.btn.addEventListener("click", () => {
+        refs.modalRef.classList.remove("is-open");
+      });
+    }
   }
-
-  let btn = document.querySelector(".lightbox__button");
-  btn.addEventListener("click", () => {
-    refs.modalRef.classList.remove("is-open");
-  });
 }
+
+const gallery = new Gallery(imagesRefs, refs);
+
+gallery.newImagesOnPage();
+gallery.addListener();
