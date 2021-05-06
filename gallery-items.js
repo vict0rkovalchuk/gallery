13732bd1625256a78a0e1f37b90f6986d7700b9b
Victor <vict02r05@gmail.com>
2021-05-06 23:58:17 +0300

    changing photos

--- gallery-items.js
+++ gallery-items.js
@@ -1,86 +1,278 @@
+// export default [
+//   {
+//     description: "rose, flower, petal",
+//     preview:
+//       "https://cdn.pixabay.com/photo/2018/01/05/16/24/rose-3063284_150.jpg",
+//     original:
+//       "https://cdn.pixabay.com/photo/2018/01/05/16/24/rose-3063284_1280.jpg",
+//   },
+//   {
+//     description: "sunflower, nature, flora",
+//     preview:
+//       "https://cdn.pixabay.com/photo/2018/01/28/11/24/sunflower-3113318_150.jpg",
+//     original:
+//       "https://cdn.pixabay.com/photo/2018/01/28/11/24/sunflower-3113318_1280.jpg",
+//   },
+//   {
+//     description: "sunflower, vase, vintage",
+//     preview:
+//       "https://cdn.pixabay.com/photo/2018/04/05/14/09/sunflower-3292932_150.jpg",
+//     original:
+//       "https://cdn.pixabay.com/photo/2018/04/05/14/09/sunflower-3292932_1280.jpg",
+//   },
+//   {
+//     description: "spring-bird",
+//     preview:
+//       "https://cdn.pixabay.com/photo/2017/05/08/13/15/spring-bird-2295434_150.jpg",
+//     original:
+//       "https://cdn.pixabay.com/photo/2017/05/08/13/15/spring-bird-2295434_1280.jpg",
+//   },
+//   {
+//     description: "lilies, yellow, flowers",
+//     preview:
+//       "https://cdn.pixabay.com/photo/2016/07/12/18/54/lilies-1512813_150.jpg",
+//     original:
+//       "https://cdn.pixabay.com/photo/2016/07/12/18/54/lilies-1512813_1280.jpg",
+//   },
+//   {
+//     description: "sunflower, flower, plant",
+//     preview:
+//       "https://cdn.pixabay.com/photo/2016/01/08/05/24/sunflower-1127174_150.jpg",
+//     original:
+//       "https://cdn.pixabay.com/photo/2016/01/08/05/24/sunflower-1127174_1280.jpg",
+//   },
+//   {
+//     description: "sunflower, yellow, flower",
+//     preview:
+//       "https://cdn.pixabay.com/photo/2016/08/28/23/24/sunflower-1627193_150.jpg",
+//     original:
+//       "https://cdn.pixabay.com/photo/2016/08/28/23/24/sunflower-1627193_1280.jpg",
+//   },
+//   {
+//     description: "yellow, natural, flower",
+//     preview:
+//       "https://cdn.pixabay.com/photo/2015/04/10/00/41/yellow-715540_150.jpg",
+//     original:
+//       "https://cdn.pixabay.com/photo/2015/04/10/00/41/yellow-715540_1280.jpg",
+//   },
+//   {
+//     description: "drip, blossom, bloom",
+//     preview:
+//       "https://cdn.pixabay.com/photo/2017/01/11/17/27/drip-1972411_150.jpg",
+//     original:
+//       "https://cdn.pixabay.com/photo/2017/01/11/17/27/drip-1972411_1280.jpg",
+//   },
+//   {
+//     description: "flower, life, crack",
+//     preview:
+//       "https://cdn.pixabay.com/photo/2015/08/13/20/06/flower-887443_150.jpg",
+//     original:
+//       "https://cdn.pixabay.com/photo/2015/08/13/20/06/flower-887443_1280.jpg",
+//   },
+//   {
+//     description: "crocus, flower, wet",
+//     preview:
+//       "https://cdn.pixabay.com/photo/2017/03/15/09/00/crocus-2145539_150.jpg",
+//     original:
+//       "https://cdn.pixabay.com/photo/2017/03/15/09/00/crocus-2145539_1280.jpg",
+//   },
+//   {
+//     description: "sunflower, bee, macro",
+//     preview:
+//       "https://cdn.pixabay.com/photo/2016/09/04/12/05/sunflower-1643794_150.jpg",
+//     original:
+//       "https://cdn.pixabay.com/photo/2016/09/04/12/05/sunflower-1643794_1280.jpg",
+//   },
+// ];
+
 export default [
   {
     description: "rose, flower, petal",
     preview:
-      "https://cdn.pixabay.com/photo/2018/01/05/16/24/rose-3063284_150.jpg",
+      "https://images.pexels.com/photos/1242764/pexels-photo-1242764.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     original:
-      "https://cdn.pixabay.com/photo/2018/01/05/16/24/rose-3063284_1280.jpg",
+      "https://images.pexels.com/photos/1242764/pexels-photo-1242764.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
   },
   {
     description: "sunflower, nature, flora",
     preview:
-      "https://cdn.pixabay.com/photo/2018/01/28/11/24/sunflower-3113318_150.jpg",
+      "https://images.pexels.com/photos/130621/pexels-photo-130621.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     original:
-      "https://cdn.pixabay.com/photo/2018/01/28/11/24/sunflower-3113318_1280.jpg",
+      "https://images.pexels.com/photos/130621/pexels-photo-130621.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
   },
   {
     description: "sunflower, vase, vintage",
     preview:
-      "https://cdn.pixabay.com/photo/2018/04/05/14/09/sunflower-3292932_150.jpg",
+      "https://images.pexels.com/photos/1624600/pexels-photo-1624600.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     original:
-      "https://cdn.pixabay.com/photo/2018/04/05/14/09/sunflower-3292932_1280.jpg",
+      "https://images.pexels.com/photos/1624600/pexels-photo-1624600.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
   },
   {
     description: "spring-bird",
     preview:
-      "https://cdn.pixabay.com/photo/2017/05/08/13/15/spring-bird-2295434_150.jpg",
+      "https://images.pexels.com/photos/1936954/pexels-photo-1936954.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     original:
-      "https://cdn.pixabay.com/photo/2017/05/08/13/15/spring-bird-2295434_1280.jpg",
+      "https://images.pexels.com/photos/1936954/pexels-photo-1936954.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
   },
   {
     description: "lilies, yellow, flowers",
     preview:
-      "https://cdn.pixabay.com/photo/2016/07/12/18/54/lilies-1512813_150.jpg",
+      "https://images.pexels.com/photos/17658/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     original:
-      "https://cdn.pixabay.com/photo/2016/07/12/18/54/lilies-1512813_1280.jpg",
+      "https://images.pexels.com/photos/17658/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
   },
   {
     description: "sunflower, flower, plant",
     preview:
-      "https://cdn.pixabay.com/photo/2016/01/08/05/24/sunflower-1127174_150.jpg",
+      "https://images.pexels.com/photos/2443590/pexels-photo-2443590.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     original:
-      "https://cdn.pixabay.com/photo/2016/01/08/05/24/sunflower-1127174_1280.jpg",
+      "https://images.pexels.com/photos/2443590/pexels-photo-2443590.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
   },
   {
     description: "sunflower, yellow, flower",
     preview:
-      "https://cdn.pixabay.com/photo/2016/08/28/23/24/sunflower-1627193_150.jpg",
+      "https://images.pexels.com/photos/6212576/pexels-photo-6212576.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     original:
-      "https://cdn.pixabay.com/photo/2016/08/28/23/24/sunflower-1627193_1280.jpg",
+      "https://images.pexels.com/photos/6212576/pexels-photo-6212576.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
   },
   {
     description: "yellow, natural, flower",
     preview:
-      "https://cdn.pixabay.com/photo/2015/04/10/00/41/yellow-715540_150.jpg",
+      "https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     original:
-      "https://cdn.pixabay.com/photo/2015/04/10/00/41/yellow-715540_1280.jpg",
+      "https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
   },
   {
     description: "drip, blossom, bloom",
     preview:
-      "https://cdn.pixabay.com/photo/2017/01/11/17/27/drip-1972411_150.jpg",
+      "https://images.pexels.com/photos/531321/pexels-photo-531321.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     original:
-      "https://cdn.pixabay.com/photo/2017/01/11/17/27/drip-1972411_1280.jpg",
+      "https://images.pexels.com/photos/531321/pexels-photo-531321.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
   },
   {
     description: "flower, life, crack",
     preview:
-      "https://cdn.pixabay.com/photo/2015/08/13/20/06/flower-887443_150.jpg",
+      "https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     original:
-      "https://cdn.pixabay.com/photo/2015/08/13/20/06/flower-887443_1280.jpg",
+      "https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
   },
   {
     description: "crocus, flower, wet",
     preview:
-      "https://cdn.pixabay.com/photo/2017/03/15/09/00/crocus-2145539_150.jpg",
+      "https://images.pexels.com/photos/443446/pexels-photo-443446.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     original:
-      "https://cdn.pixabay.com/photo/2017/03/15/09/00/crocus-2145539_1280.jpg",
+      "https://images.pexels.com/photos/443446/pexels-photo-443446.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
   },
   {
     description: "sunflower, bee, macro",
     preview:
-      "https://cdn.pixabay.com/photo/2016/09/04/12/05/sunflower-1643794_150.jpg",
+      "https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     original:
-      "https://cdn.pixabay.com/photo/2016/09/04/12/05/sunflower-1643794_1280.jpg",
+      "https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "",
+    preview:
+      "https://images.pexels.com/photos/1416900/pexels-photo-1416900.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/1416900/pexels-photo-1416900.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "",
+    preview:
+      "https://images.pexels.com/photos/46253/mt-fuji-sea-of-clouds-sunrise-46253.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/46253/mt-fuji-sea-of-clouds-sunrise-46253.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "",
+    preview:
+      "https://images.pexels.com/photos/1146708/pexels-photo-1146708.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/1146708/pexels-photo-1146708.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "",
+    preview:
+      "https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "",
+    preview:
+      "https://images.pexels.com/photos/38136/pexels-photo-38136.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/38136/pexels-photo-38136.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "",
+    preview:
+      "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "",
+    preview:
+      "https://images.pexels.com/photos/1110656/pexels-photo-1110656.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/1110656/pexels-photo-1110656.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "",
+    preview:
+      "https://images.pexels.com/photos/1485452/pexels-photo-1485452.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/1485452/pexels-photo-1485452.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "",
+    preview:
+      "https://images.pexels.com/photos/1271380/pexels-photo-1271380.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/1271380/pexels-photo-1271380.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "",
+    preview:
+      "https://images.pexels.com/photos/2213443/pexels-photo-2213443.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/2213443/pexels-photo-2213443.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "",
+    preview:
+      "https://images.pexels.com/photos/1486976/pexels-photo-1486976.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/1486976/pexels-photo-1486976.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "",
+    preview:
+      "https://images.pexels.com/photos/490411/pexels-photo-490411.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/490411/pexels-photo-490411.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "",
+    preview:
+      "https://images.pexels.com/photos/592077/pexels-photo-592077.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/592077/pexels-photo-592077.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "",
+    preview:
+      "https://images.pexels.com/photos/772429/pexels-photo-772429.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/772429/pexels-photo-772429.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "",
+    preview:
+      "https://images.pexels.com/photos/248771/pexels-photo-248771.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/248771/pexels-photo-248771.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
   },
 ];
